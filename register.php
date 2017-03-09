01f5b109edd8c5af3f7d6aa7a240ce8d5435ab5c
Anis Junior <anismackendy@yahoo.fr>
2017-03-09 07:54:33 -0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php 
+$host = "jnc6n3xpbgc3kek2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$user = "gexievz51r0py9g1v";
+$db = "lhgjmfsupyod65vz";
+$pw = "rt3q65w92v53w29q";
+
+ ?>
+
+ <!--<?php 
+//$host = "localhost";
+//$db = "examen";
+//$user = "root";
+//$pw = "";
+
+ ?>
+
+ 
